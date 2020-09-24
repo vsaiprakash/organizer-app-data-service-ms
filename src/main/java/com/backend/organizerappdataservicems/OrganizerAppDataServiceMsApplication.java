131594d83b7f3e55774a88d41ee13bd2fb13136a
Vukkum Sai Prakash <vukkumsp@gmail.com>
2020-09-24 20:31:08 +0530

    adding project init

---
+++ src/main/java/com/backend/organizerappdataservicems/OrganizerAppDataServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.organizerappdataservicems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerAppDataServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizerAppDataServiceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/organizerappdataservicems/OrganizerAppDataServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.organizerappdataservicems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerAppDataServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
